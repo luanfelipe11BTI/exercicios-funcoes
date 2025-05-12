d2168fa0bd06e0f3c03b331e32be5880d5df4ca6
luanfelipe11BTI ribeiro <kreekshotpc69@gmail.com>
2025-05-12 11:32:53 +0000

    resoluçao do exercicio 1001 - extremamente básic

---
+++ pages/extremamente_basico.js
@@ -0,0 +1,12 @@
+function somar (a, b){
+    let x= a+b  
+    return x
+}
+
+let soma1= somar(10,9)
+let soma2= somar(-10,4)
+let soma3= somar(15,-7)
+
+console.log(`x1 = ${soma1}`)
+console.log(`x2 = ${soma2}`)
+console.log(`x3 = ${soma3}`)
